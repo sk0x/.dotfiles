6b866e1f950ec45dbf9aa83145e0914836282338
sk0x <you@example.com>
2025-07-15 10:17:43 +0530

    few more things

--- installation.sh
+++ installation.sh
@@ -14,22 +14,19 @@ fi
 
 # installing apps
 sudo pacman -S stow zsh fzf tmux neovim
-
-# Change shell to zsh
-chsh -s $(which zsh)
-
-# installing ohmyzsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-exit
-
 # install browser
 yay -S brave-bin google-chrome zen-browser-bin
-
-
 # install docker
 sudo pacman -S docker
 sudo systemctl start docker.service
 sudo systemctl enable docker.service
 sudo usermod -aG docker $USER
+
+# Change shell to zsh
+chsh -s $(which zsh)
+# installing ohmyzsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+exit
+
+
 
